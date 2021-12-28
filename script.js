c49411d49c5f0571cf9c7fbb364a74f79e553a26
André Rodrigues <andre360.ars@gmail.com>
2021-12-27 22:41:28 -0300

    Requisito 3 - clicar no botão adiciona as palavras à carta

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const letter = document.getElementById('carta-gerada')
+const text = document.getElementById('carta-texto')
+const letterCreator = document.getElementById('criar-carta')
 
+// Requisito 3
+
+function letterGenerator() {
+    let splitedText = text.value.split(' ');
+    
+    for (let i = 0; i < splitedText.length; i += 1) {
+       let word = document.createElement('span');
+
+       word.innerText = splitedText[i];
+       letter.appendChild(word)
+    }
+}
+
+letterCreator.addEventListener('click', letterGenerator)
+// essa é a minha carta !!!
